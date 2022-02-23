2ed5d05e1744e1745505c1a1a348616980405fd4
ucdale <ucdale@gmail.com>
2022-02-23 16:20:50 +0100

    Cambio home

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import useFetch from './useFetch';
 
-function App() {
+const App =() => {
   const [data, loading] = useFetch('/api');
 
   return (
@@ -10,16 +10,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ciao per ora questa sarà la home page
         </p>
         <p>{loading ? 'loading...'  : data.message}</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.google.it/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwjT1ICXjpb2AhWJRPEDHa9yBrYQFnoECAsQAQ&url=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FApplication_programming_interface&usg=AOvVaw0cIDNPqqFEU9OB-AdxhuVa"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Cosa sono le API
         </a>
       </header>
     </div>
