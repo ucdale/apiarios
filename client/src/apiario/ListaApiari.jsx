f3fcdc28dc12a48d8c1bab5e2547c62ea73efdf9
ucdale <ucdale@gmail.com>
2022-02-25 19:22:03 +0100

    Corretto path relativo per lista apiari

--- client/src/apiario/ListaApiari.jsx
+++ client/src/apiario/ListaApiari.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import logo from './logo.svg';
-import './App.css';
-import useFetch from './useFetch';
+import logo from '../logo.svg';
+import '../App.css';
+import useFetch from '../useFetch';
 
 const ListaApiari =() => {
   const [listaApiari, setListaApiari] = useState([]);
