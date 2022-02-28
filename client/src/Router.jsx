b753e9dd119b0bfe34365639f0cc1394007aeabd
Alessandro Caregnato <alessandro.caregnato@cgn.it>
2022-02-28 12:17:08 +0100

    Correzione react router

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -2,28 +2,20 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ListaApiari from './apiario/ListaApiari';
 import Home from './Home';
+import { Link } from "react-router-dom";
 
 const Router = () => (
-    <BrowserRouter>
         <Routes>
             <Route
             exact
-            component={() => (
-                <Home />
-            )}
             path="/"
+            element={ <Home/> }
             />
-        </Routes>
-        <Routes>
             <Route
-            exact
-            component={() => (
-                <ListaApiari />
-            )}
             path="/lista"
+            element={<ListaApiari />}
             />
         </Routes>
-    </BrowserRouter>
 );
 
 export default Router;
