49614f47b808c518ed1fa666ad83c4bc4d7e66e2
ucdale <ucdale@gmail.com>
2022-03-06 13:08:50 +0100

    Prova collegamento a db

--- server/config.js
+++ server/config.js
@@ -1,16 +1,94 @@
 const config = {};
 
-config.host = process.env.HOST || "https://apiarios.documents.azure.com:443/";
-config.authKey =
-  process.env.AUTH_KEY || "K40VAzamG2f2NASgyIXD2GGKdUd2LGZxGKvOGUab0sEgxVR7gkHimB9MJI3XC6ykMHus3tcAzCLS2ST3YHZzzQ==";
+// prod
+config.host = "https://apiarios.documents.azure.com:443/";
+config.authKey = "K40VAzamG2f2NASgyIXD2GGKdUd2LGZxGKvOGUab0sEgxVR7gkHimB9MJI3XC6ykMHus3tcAzCLS2ST3YHZzzQ==";
 config.databaseId = "Apiarios";
-config.containerId = "Items";
+config.containerId = "Apiari";
 
-if (config.host.includes("https://localhost:")) {
-  console.log("Local environment detected");
-  console.log("WARNING: Disabled checking of self-signed certs. Do not have this code in production.");
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-  console.log(`Go to http://localhost:${process.env.PORT || '8080'} to try the sample.`);
+// locale
+// config.endpoint = 'https://localhost:8081'
+// config.key = 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=='
+// config.databaseId = "Apiarios";
+// config.containerId = "Apiari";
+
+
+config.database = {
+  id: 'apiarios'
 }
+
+config.container = {
+  id: 'apiari'
+}
+
+config.items = {
+  IsolaAugusta: {
+    id: 'IsolaAgusta.1',
+    zona: 'Palazzolo',
+    casette: [
+      {
+        numero: '1',
+        colore: 'blu',
+        stato: [
+          {
+            data: '21/04/2021',
+            isReginaTrovata: true,
+            isCovataFresca: true,
+            melari:[
+              {primo: "un po"},
+              {secondo: "pieno"},
+              {terzo: "vuoto"}
+            ]
+          }
+        ]
+      },
+      {
+        numero: '2',
+        colore: 'rossa',
+        stato: [
+          {
+            data: '21/04/2021',
+            isReginaTrovata: true,
+            isCovataFresca: true,
+            melari:[
+              {primo: "vuoto"},
+              {secondo: "pieno"},
+              {terzo: "vuoto"}
+            ]
+          }
+        ]
+      }
+    ],
+  },
+  Gorgo: {
+    id: 'Gorgo.1',
+    zona: 'Gorgo',
+    casette: [
+      {
+        numero: '1',
+        colore: 'blu',
+        stato: [
+          {
+            data: '21/04/2021',
+            isReginaTrovata: true,
+            isCovataFresca: true,
+            melari:[
+              {primo: "pieno"},
+              {secondo: "pieno"},
+              {terzo: "meta"}
+            ]
+          }
+        ]
+      }
+    ],
+  },
+}
+
+// if (config.host.includes("https://localhost:")) {
+//   console.log("Local environment detected");
+//   console.log("WARNING: Disabled checking of self-signed certs. Do not have this code in production.");
+//   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+//   console.log(`Go to http://localhost:${process.env.PORT || '8080'} to try the sample.`);
+// }
 
 module.exports = config;
--- server/index.js
+++ server/index.js
@@ -2,62 +2,37 @@ const CosmosClient = require("@azure/cosmos").CosmosClient;
 const config = require("./config");
 const TaskList = require("./controllers/tasklist");
 const TaskDao = require("./models/taskDao");
+const url = require('url')
 
-const express = require('express'),
-      path    = require('path'),
-      route   = require('./route.js'),
-      app     = express(),
-      port    = process.env.PORT || 8080;
-// const path = require("path");
+const express = require('express');
+const  path    = require('path');
+const  route   = require('./route.js');
+const  app     = express();
+const  port    = process.env.PORT || 8080;
 const logger = require("morgan");
-// const cookieParser = require("cookie-parser");
-// const bodyParser = require("body-parser");
 
-// const app = express();
+const endpoint = config.endpoint
+const key = config.key
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+const databaseId = config.database.id
+const containerId = config.container.id
+const partitionKey = { kind: 'Hash', paths: ['/Country'] }
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger("dev"));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+const options = {
+      endpoint: endpoint,
+      key: key,
+      userAgentSuffix: 'Apiarios'
+    };
 
-//Todo App:
-const cosmosClient = new CosmosClient({
-  endpoint: config.host,
-  key: config.authKey,
-});
-const taskDao = new TaskDao(
-  cosmosClient,
-  config.databaseId,
-  config.containerId
-);
-const taskList = new TaskList(taskDao);
-taskDao
-  .init((err) => {
-    console.error(err);
-  })
-  .catch((err) => {
-    console.error(err);
-    console.error(
-      "Shutting down because there was an error settinig up the database."
-    );
-    process.exit(1);
-  });
+const client = new CosmosClient(options)
 
-app.get("/", (req, res, next) => taskList.showTasks(req, res).catch(next));
-app.post("/addtask", (req, res, next) =>
-  taskList.addTask(req, res).catch(next)
-);
-app.post("/completetask", (req, res, next) =>
-  taskList.completeTask(req, res).catch(next)
-);
-app.set("view engine", "jade");
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// route(app);
+
+// app.listen(port);
+
+console.log(`API server is listening on port:${port}`);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -74,7 +49,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.sendStatus(err.status || 500)
 });
 
 // module.exports = app;
@@ -86,3 +61,182 @@ route(app);
 app.listen(port);
 
 console.log(`API server is listening on port:${port}`);
+
+//@ts-check
+
+
+/**
+ * Create the database if it does not exist
+ */
+async function createDatabase() {
+  const { database } = await client.databases.createIfNotExists({
+    id: databaseId
+  })
+  console.log(`Created database:\n${database.id}\n`)
+}
+
+/**
+ * Read the database definition
+ */
+async function readDatabase() {
+  const { resource: databaseDefinition } = await client
+    .database(databaseId)
+    .read()
+  console.log(`Reading database:\n${databaseDefinition.id}\n`)
+}
+
+/**
+ * Create the container if it does not exist
+ */
+async function createContainer() {
+  const { container } = await client
+    .database(databaseId)
+    .containers.createIfNotExists(
+      { id: containerId, partitionKey },
+      { offerThroughput: 400 }
+    )
+  console.log(`Created container:\n${config.container.id}\n`)
+}
+
+/**
+ * Read the container definition
+ */
+async function readContainer() {
+  const { resource: containerDefinition } = await client
+    .database(databaseId)
+    .container(containerId)
+    .read()
+  console.log(`Reading container:\n${containerDefinition.id}\n`)
+}
+
+/**
+ * Scale a container
+ * You can scale the throughput (RU/s) of your container up and down to meet the needs of the workload. Learn more: https://aka.ms/cosmos-request-units
+ */
+async function scaleContainer() {
+  const { resource: containerDefinition } = await client
+    .database(databaseId)
+    .container(containerId)
+    .read()
+  const {resources: offers} = await client.offers.readAll().fetchAll();
+
+  const newRups = 500;
+  for (var offer of offers) {
+    if (containerDefinition._rid !== offer.offerResourceId)
+    {
+        continue;
+    }
+    offer.content.offerThroughput = newRups;
+    const offerToReplace = client.offer(offer.id);
+    await offerToReplace.replace(offer);
+    console.log(`Updated offer to ${newRups} RU/s\n`);
+    break;
+  }
+}
+
+/**
+ * Create family item if it does not exist
+ */
+async function createApiarioItem(itemBody) {
+  const { item } = await client
+    .database(databaseId)
+    .container(containerId)
+    .items.upsert(itemBody)
+  console.log(`Created apiario item with id:\n${itemBody.id}\n`)
+}
+
+/**
+ * Query the container using SQL
+ */
+async function queryContainer() {
+  console.log(`Querying container:\n${config.container.id}`)
+
+  // query to return all children in a family
+  // Including the partition key value of lastName in the WHERE filter results in a more efficient query
+  const querySpec = {
+    query: 'SELECT VALUE r.children FROM root r WHERE r.lastName = @lastName',
+    parameters: [
+      {
+        name: '@lastName',
+        value: 'Andersen'
+      }
+    ]
+  }
+
+  const { resources: results } = await client
+    .database(databaseId)
+    .container(containerId)
+    .items.query(querySpec)
+    .fetchAll()
+  for (var queryResult of results) {
+    let resultString = JSON.stringify(queryResult)
+    console.log(`\tQuery returned ${resultString}\n`)
+  }
+}
+
+/**
+ * Replace the item by ID.
+ */
+async function replaceFamilyItem(itemBody) {
+  console.log(`Replacing item:\n${itemBody.id}\n`)
+  // Change property 'grade'
+  itemBody.children[0].grade = 6
+  const { item } = await client
+    .database(databaseId)
+    .container(containerId)
+    .item(itemBody.id, itemBody.Country)
+    .replace(itemBody)
+}
+
+/**
+ * Delete the item by ID.
+ */
+async function deleteFamilyItem(itemBody) {
+  await client
+    .database(databaseId)
+    .container(containerId)
+    .item(itemBody.id, itemBody.Country)
+    .delete(itemBody)
+  console.log(`Deleted item:\n${itemBody.id}\n`)
+}
+
+/**
+ * Cleanup the database and collection on completion
+ */
+async function cleanup() {
+  await client.database(databaseId).delete()
+}
+
+/**
+ * Exit the app with a prompt
+ * @param {string} message - The message to display
+ */
+function exit(message) {
+  console.log(message)
+  console.log('Press any key to exit')
+  process.stdin.setRawMode(true)
+  process.stdin.resume()
+  process.stdin.on('data', process.exit.bind(process, 0))
+}
+
+createDatabase()
+  .then(() => readDatabase())
+  .then(() => createContainer())
+  .then(() => readContainer())
+  .then(() => scaleContainer())
+  .then(() => createApiarioItem(config.items.IsolaAugusta))
+  .then(() => createApiarioItem(config.items.Gorgo))
+  .then(() => queryContainer())
+  // .then(() => replaceFamilyItem(config.items.Andersen))
+  // .then(() => queryContainer())
+  // .then(() => deleteFamilyItem(config.items.Andersen))
+  .then(() => {
+    // exit(`Completed successfully`)
+    console.log(`Completed successfully`)
+    console.log(`API server is listening on port:${port}`);
+  })
+  .catch(error => {
+    // exit(`Completed with error ${JSON.stringify(error)}`)
+    console.log(`Completed with error`)
+    console.log(`API server is listening on port:${port}`);
+  })
